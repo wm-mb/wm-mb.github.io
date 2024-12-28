b5040004758dcaecc02bb5469d820ef110e08005
wm-mb <braucks.w@gmail.com>
2024-12-28 17:30:57 -0300

    Consertando dimensões do autoscroll. Tentando melhorar mobile.

--- center-scroll.js
+++ center-scroll.js
@@ -1,3 +1,3 @@
-window.scrollTo({top: document.body.scrollWidth/2 - window.innerWidth/2,
-        left: document.body.scrollHeight/2 - window.innerHeight/2,
+window.scrollTo({top: document.body.scrollHeight/2 - window.innerHeight/2,
+        left: document.body.scrollWidth/2 - window.innerWidth/2,
         behavior: "smooth",})
